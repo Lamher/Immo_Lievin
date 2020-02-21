7cf0016566d7618e5c169573133665285dd59956
Lamorski <philippe.lamorskipro@gmail.com>
2020-02-21 10:15:32 +0100

    make template header public

--- app/views/front/partial/header.php
+++ app/views/front/partial/header.php
@@ -21,7 +21,46 @@
     <div class="btnProposer btnLg align-center  col-lg-1">
         <button type="button" class="btn btn-light">Proposer un bien</button>
     </div>
+
 </div>
+<!--navbar pour desktop-->
+    <div class="menu-lg d-flex justify-content-center">
+        <a class="nav-link menu-lg text-dark" href="notre_agence.php">Notre agence</a>
+        <a class="nav-link menu-lg text-dark" href="location.php">Location</a>
+        <a class="nav-link menu-lg text-dark" href="ventes.php">Ventes</a></div>
+
+</div>
+<!--Navbar pour telehonne-->
+
+<nav class="menu-sm navbar-light dark lighten-4 mb-4">
+
+    <!-- Navbar brand -->
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20"
+            aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="animated-icon1"><span></span><span></span><span></span></div>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent20">
+
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-center" href="notreagence.php">Notre agence</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="location.php">Locations</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" href="ventes.php">Ventes</a>
+            </li>
+        </ul>
+
+
+    </div>
+    <!-- Collapsible content -->
 
 
 
