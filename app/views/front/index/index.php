2afc297c4b6867df5339358fbd5d48102ef3c1ae
Cleyam <cleyam@gmail.com>
2020-03-03 09:29:30 +0100

    wtf

--- app/views/front/index/index.php
+++ app/views/front/index/index.php
@@ -2,16 +2,13 @@
     <div id='carousel' class='carousel slide' data-ride='carousel'>
         <div class='carousel-inner'>
             <div class='carousel-item active'>
-                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison1.png' ?>'
-                     alt='First slide'>
+                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison1.png' ?>' alt='First slide'>
             </div>
             <div class='carousel-item'>
-                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison2.png' ?>'
-                     alt='Second slide'>
+                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison2.png' ?>' alt='Second slide'>
             </div>
             <div class='carousel-item'>
-                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison3.png' ?>'
-                     alt='Third slide'>
+                <img class='img-fluid img-carousel' src='<?= BASE_URI . 'images/img_carousel/maison3.png' ?>' alt='Third slide'>
             </div>
         </div>
         <a class='carousel-control-prev' href='#carousel' role='button' data-slide='prev'>
